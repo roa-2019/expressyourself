a8c3014e931dcbf5d7fb10178e8adef4c432c8c4
rosedhlakama <rosejembe@live.com>
2019-08-15 16:17:21 +1200

    setup routes and server

---
+++ routes.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+const fs = require('fs')
+
+module.exports = router
+
+router.get('/', (req, res)=> {
+  res.render('/index')
+  console.log("This is working!")
+})
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const hbs = require('express-handlebars')
+const routes = require('./routes')
+const server = express()
+
+
+module.exports = server
+
+server.engine('hbs', hbs({
+  defaultLayout: true,
+  extname: 'hbs',
+}))
+
+server.set('view engine', 'hbs')
+server.use(express.urlencoded({extended: false}))
