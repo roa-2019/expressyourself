315f7bd9b55cabfa349f1873b26382b83b9897a6
Sylvia Gilbert-Potts <51007051+sylvia-gp@users.noreply.github.com>
2019-08-16 14:36:52 +1200

    fix rating and comment syntax

--- routes.js
+++ routes.js
@@ -9,38 +9,39 @@ router.get('/', (req, res) => {
   res.render('./partials/home')
 })
 
-router.get('/category/:id', (req, res)=> {
+router.get('/category/:id', (req, res) => {
   const categ = req.params.id
   const array = data.places
 
   const newArray = array.filter(place => place.category == categ)
 
-  const stuff = {places: newArray}
+  const stuff = { places: newArray }
   console.log(newArray, categ)
   res.render('./partials/category.hbs', stuff)
 })
 
-router.get('/category/comments/:id', (req, res)=> {
+router.get('/category/comments/:id', (req, res) => {
   const id = req.params.id - 1
   const place = data.places[id]
   res.render('./partials/place-comments.hbs', place)
 })
 
 router.post('/category/comments/:id', (req, res) => {
-  const id = req.params.id
-  const comments = data.places[id]
+  const id = parseInt(req.params.id - 1)
+  const allComments = data.places[id].comments
+  console.log(id)
   const newComment = {
-    text: req.body.text
-    rating: Number(req.body.rating)
+    text: req.body.text,
+    rating: req.body.rating
   }
   
-  comments.push(newComment)
+  allComments.push(newComment)
 
   
   fs.writeFile('./data.json', JSON.stringify(data), function(error) {
       if (error) {
         return res.status(500).send('Oh no! Somethings not right!') 
       } 
-        return res.redirect('/category/comments/' + id)
+        return res.redirect('/category/comments/' + (id +1))
       })
 })
